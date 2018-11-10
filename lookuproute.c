4c2a45206eaafa20fffecd23857af2e122b0d0b9
孙亚男 <2016011285@secoder.net>
2018-11-10 14:26:58 +0800

    complete insert & lookup

--- lookuproute.c
+++ lookuproute.c
@@ -3,17 +3,92 @@
 
 int insert_route(unsigned long  ip4prefix,unsigned int prefixlen,char *ifname,unsigned int ifindex,unsigned long  nexthopaddr)
 {
-	printf("insert_route()");
-			
+	printf("------------------------------\n");
+	printf("insert_route()\n");
+	struct route *p = (struct route*)malloc(sizeof(struct route));
+	memset(p, 0, sizeof(struct route));
+	p->nexthop = (struct nexthop*)malloc(sizeof(struct nexthop));
+	memset(p->nexthop, 0, sizeof(struct nexthop));
+	p->next = route_table->next;
+	route_table->next = p;
+	p->prefixlen = prefixlen;
+	p->ip4prefix.s_addr = ip4prefix;
+	
+	p->nexthop->ifindex = ifindex;
+	p->nexthop->ifname = ifname;
+	p->nexthop->nexthopaddr.s_addr = nexthopaddr;
+
+	return 1;
 }
 
 int lookup_route(struct in_addr dstaddr,struct nextaddr *nexthopinfo)
 {
-	printf("lookup_route()");
+	printf("------------------------------\n");
+	printf("lookup_route()\n");
+	
+	struct route *p = route_table->next;
+	//check_node(p);
+	//check_node(p->next);
+	while (p != NULL) {
+		if (p->ip4prefix.s_addr == dstaddr.s_addr) {
+			check_node(p->next);
+			printf("find\n");
+			nexthopinfo->ifname = p->nexthop->ifname;
+			nexthopinfo->prefixl = p->nexthop->ifindex;
+			nexthopinfo->ipv4addr.s_addr = p->nexthop->nexthopaddr.s_addr;
+			return 1;
+		}
+		else {
+			p = p->next;
+		}
+	}
+	printf("not find\n");
+	return 0;
 }
 
 int delete_route(struct in_addr dstaddr,unsigned int prefixlen)
 {
-	printf("delete_route()");
+	printf("------------------------------\n");
+	printf("delete_route()\n");
+	struct route *p = route_table;
+	
+}
+
+void check_route_table() {
+	printf("------------------------------\n");
+	printf("check_route_table()\n");
+	struct route *p = route_table;
+	while (p->next != NULL) {
+		check_node(p->next);
+		p = p->next;
+	}
+}
+
+void check_node(struct route* p){
+	printf("------------------------------\n");
+	printf("check_node\n");
+	if (p != NULL) {
+		printf("not null\n");
+		printf("%s\n", p->nexthop->ifname);
+		printf("%u\n", p->ip4prefix.s_addr);
+		//printf("p->prefixlen:");
+		//printf(p->prefixlen);
+		//printf("\n");
+		//printf("p->ip4prefix.s_addr");
+		//printf(p->ip4prefix.s_addr);
+		//printf("\n");
+		//printf("p->nexthop->ifindex");
+		//printf(p->nexthop->ifindex);
+		//printf("\n");
+		//printf("p->nexthop->ifname");
+		//printf(p->nexthop->ifname);
+		//printf("\n");
+		//printf("p->nexthop->nexthopaddr.in_addr_t");
+		//printf(p->nexthop->nexthopaddr.s_addr);
+		//printf("\n");
+	}
+	else {
+		printf("node is null");
+	}
 }
 
--- main.c
+++ main.c
@@ -82,105 +82,127 @@ int main()
 			return -1;
 	}
 	memset(route_table,0,sizeof(struct route));
-
+	printf("before insert\n");
 
 	{
 		
 	//调用添加函数insert_route往路由表里添加直连路由
-	
+		insert_route(0x305a8c0, 24, "eth1", 4, 0x204a8c0);
+		insert_route(0x365a8c0, 24, "eth2", 4, 0x204a8c0);
+		insert_route(0x308a8c0, 24, "eth3", 4, 0x204a8c0);
+		insert_route(0x343a8c0, 24, "eth4", 4, 0x204a8c0);
+		insert_route(0x324a8c0, 24, "eth5", 4, 0x204a8c0);
+		check_route_table();
+
+		struct in_addr tmp;
+		struct nextaddr *nexthopinfo;
+		nexthopinfo = (struct nextaddr *)malloc(sizeof(struct nextaddr));
+		memset(nexthopinfo,0,sizeof(struct nextaddr));
+		
+		tmp.s_addr = 0x343a8c0;
+		lookup_route(tmp, nexthopinfo);
+		tmp.s_addr = 0x324a8c0;
+		lookup_route(tmp, nexthopinfo);
+		tmp.s_addr = 0x34458c0;
+		lookup_route(tmp, nexthopinfo);
+		tmp.s_addr = 0x308a8c0;
+		lookup_route(tmp, nexthopinfo);
+
+
 	}
 
-	//创建线程去接收路由信息
-	int pd;
-	pd = pthread_create(&tid,NULL,thr_fn,NULL);
+	//-------------------------------------------------------------------------------------------------------
+	////创建线程去接收路由信息
+	//int pd;
+	//pd = pthread_create(&tid,NULL,thr_fn,NULL);
 
 
-	while(1)
-	{
-		//接收ip数据包模块
-		recvlen=recv(recvfd,skbuf,sizeof(skbuf),0);
-		if(recvlen>0)
-		{
-		
-			ip_recvpkt = (struct ip *)(skbuf+ETHER_HEADER_LEN);
-			
-			//192.168.1.10是测试服务器的IP，现在测试服务器IP是192.168.1.10到192.168.1.80.
-			//使用不同的测试服务器要进行修改对应的IP。然后再编译。
-			//192.168.6.2是测试时候ping的目的地址。与静态路由相对应。
- 			if(ip_recvpkt->ip_src.s_addr == inet_addr("192.168.1.10") && ip_recvpkt->ip_dst.s_addr == inet_addr("192.168.6.2") )
-			{
-				//分析打印ip数据包的源和目的ip地址
-			//	analyseIP(ip_recvpkt);
+	//while(1)
+	//{
+	//	//接收ip数据包模块
+	//	recvlen=recv(recvfd,skbuf,sizeof(skbuf),0);
+	//	if(recvlen>0)
+	//	{
+	//	
+	//		ip_recvpkt = (struct ip *)(skbuf+ETHER_HEADER_LEN);
+	//		
+	//		//192.168.1.10是测试服务器的IP，现在测试服务器IP是192.168.1.10到192.168.1.80.
+	//		//使用不同的测试服务器要进行修改对应的IP。然后再编译。
+	//		//192.168.6.2是测试时候ping的目的地址。与静态路由相对应。
+ //			if(ip_recvpkt->ip_src.s_addr == inet_addr("192.168.1.10") && ip_recvpkt->ip_dst.s_addr == inet_addr("192.168.6.2") )
+	//		{
+	//			//分析打印ip数据包的源和目的ip地址
+	//		//	analyseIP(ip_recvpkt);
 
-				int s;
-				memset(data,0,1480);			
-				for(s=0;s<1480;s++)
-				{
-					data[s]=skbuf[s+34];
-				}
+	//			int s;
+	//			memset(data,0,1480);			
+	//			for(s=0;s<1480;s++)
+	//			{
+	//				data[s]=skbuf[s+34];
+	//			}
 
-				 
-					// 校验计算模块
-					struct _iphdr *iphead;
-					int c=0;
+	//			 
+	//				// 校验计算模块
+	//				struct _iphdr *iphead;
+	//				int c=0;
 
-					iphead=(struct _iphdr *)malloc(sizeof(struct _iphdr));
-					
-					{
+	//				iphead=(struct _iphdr *)malloc(sizeof(struct _iphdr));
+	//				
+	//				{
 
-					//调用校验函数check_sum，成功返回1
-					}
-					if(c ==1)
-					{
-							printf("checksum is ok!!\n");
-					}else
-					{
-							printf("checksum is error !!\n");
-							return -1;
-					}
+	//				//调用校验函数check_sum，成功返回1
+	//				}
+	//				if(c ==1)
+	//				{
+	//						printf("checksum is ok!!\n");
+	//				}else
+	//				{
+	//						printf("checksum is error !!\n");
+	//						return -1;
+	//				}
 
-					{
-						
-					//调用计算校验和函数count_check_sum，返回新的校验和 
-					} 
+	//				{
+	//					
+	//				//调用计算校验和函数count_check_sum，返回新的校验和 
+	//				} 
 
 
-					//查找路由表，获取下一跳ip地址和出接口模块
-					struct nextaddr *nexthopinfo;
-					nexthopinfo = (struct nextaddr *)malloc(sizeof(struct nextaddr));
-					memset(nexthopinfo,0,sizeof(struct nextaddr));
-					{
-						
-					//调用查找路由函数lookup_route，获取下一跳ip地址和出接口
-					}
+	//				//查找路由表，获取下一跳ip地址和出接口模块
+	//				struct nextaddr *nexthopinfo;
+	//				nexthopinfo = (struct nextaddr *)malloc(sizeof(struct nextaddr));
+	//				memset(nexthopinfo,0,sizeof(struct nextaddr));
+	//				{
+	//					
+	//				//调用查找路由函数lookup_route，获取下一跳ip地址和出接口
+	//				}
 
-					
-					//arp find
-					struct arpmac *srcmac;
-					srcmac = (struct arpmac*)malloc(sizeof(struct arpmac));
-					memset(srcmac,0,sizeof(struct arpmac));
-					{
-						
-					//调用arpGet获取下一跳的mac地址		
-					}
+	//				
+	//				//arp find
+	//				struct arpmac *srcmac;
+	//				srcmac = (struct arpmac*)malloc(sizeof(struct arpmac));
+	//				memset(srcmac,0,sizeof(struct arpmac));
+	//				{
+	//					
+	//				//调用arpGet获取下一跳的mac地址		
+	//				}
 
-					//send ether icmp
-					{
-					//调用ip_transmit函数   填充数据包，通过原始套接字从查表得到的出接口(比如网卡2)将数据包发送出去
-					//将获取到的下一跳接口信息存储到存储接口信息的结构体ifreq里，通过ioctl获取出接口的mac地址作为数据包的源mac地址
-					//封装数据包：
-					//<1>.根据获取到的信息填充以太网数据包头，以太网包头主要需要源mac地址、目的mac地址、以太网类型eth_header->ether_type = htons(ETHERTYPE_IP);
-					//<2>.再填充ip数据包头，对其进行校验处理；
-					//<3>.然后再填充接收到的ip数据包剩余数据部分，然后通过raw socket发送出去
-					}
-			}
-		
-			
+	//				//send ether icmp
+	//				{
+	//				//调用ip_transmit函数   填充数据包，通过原始套接字从查表得到的出接口(比如网卡2)将数据包发送出去
+	//				//将获取到的下一跳接口信息存储到存储接口信息的结构体ifreq里，通过ioctl获取出接口的mac地址作为数据包的源mac地址
+	//				//封装数据包：
+	//				//<1>.根据获取到的信息填充以太网数据包头，以太网包头主要需要源mac地址、目的mac地址、以太网类型eth_header->ether_type = htons(ETHERTYPE_IP);
+	//				//<2>.再填充ip数据包头，对其进行校验处理；
+	//				//<3>.然后再填充接收到的ip数据包剩余数据部分，然后通过raw socket发送出去
+	//				}
+	//		}
+	//	
+	//		
 
-		}
-	}
+	//	}
+	//}
 
-	close(recvfd);	
+	//close(recvfd);	
 	return 0;
 }
 
